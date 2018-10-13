25a7513febd9e4fcde6daea7d72adff13f4ed78a
boldizsarBalogh <bal.boldizsar@gmail.com>
2018-10-13 14:15:47 +0200

    implemented the NearbyElementFinderApp

---
+++ ElementFinder.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class ElementFinder {
+	int[][] multi = new int[][]{
+        { 2, 0, 4, 1241, 12, 5, 11, 1110, -42, 424, 1, 12323 },
+        { 1, 3, 5, 7 },
+        { 321, 320, 32, 3, 41241, -11, -12, -13, -66, -688 }
+	};
+	public int[] nearby(int x, int y, int range){
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		int NearbyCounter = -(range);
+		for(int i = 0; i < range * 2;i++){
+			System.out.println("NearbyCounter=" + NearbyCounter);
+			if (NearbyCounter == 0){
+				NearbyCounter += 1;
+			}
+			try{
+					result.add(multi[x][y + NearbyCounter]);
+			}  catch (IndexOutOfBoundsException e) {
+				System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+			}
+			NearbyCounter += 1;
+		}
+		System.out.println(result);
+		int [] arrayResult = new int[result.size()]; 
+		for(int i = 0; i < result.size();i++){
+			arrayResult[i] = result.get(i);
+		}
+		return arrayResult;
+	}
+}
---
+++ NearbyElementFinderApp.java
@@ -0,0 +1,6 @@
+public class NearbyElementFinderApp {
+	public static void main(String[] args) {
+		ElementFinder finder = new ElementFinder();
+		finder.nearby(1,3,5);
+	}
+}

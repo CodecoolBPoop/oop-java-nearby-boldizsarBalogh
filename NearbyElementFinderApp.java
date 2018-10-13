3be6ea567c5b14fa0808d082160d0000f90e948d
boldizsarBalogh <bal.boldizsar@gmail.com>
2018-10-13 14:52:05 +0200

    added user input for better functionality and testing purposes

--- NearbyElementFinderApp.java
+++ NearbyElementFinderApp.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class NearbyElementFinderApp {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		ElementFinder finder = new ElementFinder();
-		finder.nearby(1,3,5);
+		
+		System.out.println("Give me the first coordinate:");
+		int firstCoordinate = scanner.nextInt();
+		System.out.println("Give me the second coordinate:");
+		int secondCoordinate = scanner.nextInt();
+		System.out.println("Give me the search range:");
+		int range = scanner.nextInt();
+		finder.nearby(firstCoordinate,secondCoordinate,range);
 	}
 }
